bd3d976ecd20d51d0742b51c9b8c91e39fbe8181
Andrew Lee <andrew120.lee@gmail.com>
2014-03-22 23:58:17 -0400

    Added hw 9 but doesn't work fully

---
+++ 09-LinkedLists2/MyLinkedList.java
@@ -0,0 +1,119 @@
+import java.io.*;
+import java.util.*;
+
+public class MyLinkedList {
+    private Node head;
+    private boolean b;
+    
+    public MyLinkedList() {
+	head = new Node("0");
+    }
+
+    public void add(String s) {
+    	Node temp = new Node(s);
+    	temp.setNext(head);
+    	head = temp;
+    }
+
+    public String toString() {
+	Node temp = head;
+	String s = "";
+	while (temp.getNext() != null){
+	    s = s + temp.getData();
+	    temp = temp.getNext();
+	}
+	return "" + head;
+    }
+
+    public void add2(int i, String s) {
+	Node tmp = head;
+	for (int j = 0;j<i;j++){
+	    if (tmp.getNext() == null && j+1 != i){
+		System.exit(0);
+	    }
+	    else {
+		tmp = tmp.getNext();
+	    }
+	}
+	Node str = new Node(s);
+	tmp.setNext(str);
+    }
+    
+    public String get(int i) {
+	Node tmp = head;
+	for (int j = 0;j<i;j++){
+	    if (tmp.getNext() == null && j+1 != i){
+		System.exit(0);
+	    }
+	    else {
+		tmp = tmp.getNext();
+	    }
+	}
+	return tmp.toString();
+    }
+
+    public String set(int i, String s){
+	Node tmp = head;
+	for (int j = 0; j<i;j++){
+	    if (tmp.getNext() == null && j+1 != i){
+		System.exit(0);
+	    }
+	    else {
+		tmp = tmp.getNext();
+	    }
+	}
+	Node old = tmp.getNext();
+	Node str = new Node(s);
+	tmp.setNext(str);
+	return old.toString();
+    }
+
+    public String remove(int i){
+	Node tmp = head;
+	for (int j = 0;j<i;j++){
+	    if (tmp.getNext() == null && j+1 != i){
+		System.exit(0);
+	    }
+	    else {
+		tmp = tmp.getNext();
+	    }
+	}
+	Node old = tmp.getNext();
+	tmp = null;
+	return old.toString();
+    }
+
+    public int find(String s){
+	b = false;
+	Node tmp = head;
+	int count = 0;
+	while (!b){
+	    if (tmp.getData().equals(s)){
+		b = true;
+		return count;
+	    }
+	    else {
+		tmp = tmp.getNext();
+	    }
+	    count = count + 1;
+	}
+	return count;
+    }
+
+    public int length(){
+	boolean end = false;
+	Node tmp = head;
+	int count = 0;
+	while (!b){
+	    if (tmp.getData().equals("0")){
+		b = true;
+		return count;
+	    }
+	    else {
+		tmp = tmp.getNext();
+	    }
+	    count = count + 1;
+	}
+	return count;
+    }
+}
