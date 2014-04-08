3d39145eb2fa15d0c795965984a95aa6eec30b9a
Andrew Lee <andrew120.lee@gmail.com>
2014-04-07 22:29:53 -0400

    Added hw 13

---
+++ 13-RPN/RPN.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.util.*;
+
+public class RPN {
+    public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	MyStack stack = new MyStack();
+	boolean yes = true;
+	System.out.println("Welcome to the RPN calculator.");
+	try {
+	    Thread.sleep(1000);
+	} catch(Exception e){}
+	System.out.println("If you choose to stop, type EXIT.");
+	try {
+	    Thread.sleep(1000);
+	} catch(Exception e){}
+	while (yes) {
+	    System.out.print("Please enter a number or operator: ");
+	    String s = sc.nextLine();
+	    if (s.equals("EXIT")) {
+		yes = false;
+		System.exit(0);
+	    }
+	    else if (s.equals("+")){
+		int a = Integer.parseInt(stack.pop());
+		int b = Integer.parseInt(stack.pop());
+		int ans = a + b;
+		stack.push(Integer.toString(ans));
+		System.out.println(stack.peek());
+	    }
+	    else if (s.equals("-")){
+		int a = Integer.parseInt(stack.pop());
+		int b = Integer.parseInt(stack.pop());
+		int ans = b - a;
+		stack.push(Integer.toString(ans));
+		System.out.println(stack.peek());
+	    }
+	    else if (s.equals("*")){
+		int a = Integer.parseInt(stack.pop());
+		int b = Integer.parseInt(stack.pop());
+		int ans = a * b;
+		stack.push(Integer.toString(ans));
+		System.out.println(stack.peek());
+	    }
+	    else if (s.equals("/")){
+		int a = Integer.parseInt(stack.pop());
+		int b = Integer.parseInt(stack.pop());
+		int ans = b / a;
+		stack.push(Integer.toString(ans));
+		System.out.println(stack.peek());
+	    }
+	    else {
+		stack.push(s);
+		System.out.println(stack.peek());
+	    }
+	}
+    }
+}
+		    
